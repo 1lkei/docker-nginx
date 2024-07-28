a2caa487fa40175e157257e4c9d49df134a161e2
ilkeiii <62320562+ilkeiii@users.noreply.github.com>
2024-07-28 21:00:39 +0800

    fix

--- get-modules.sh
+++ get-modules.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # curl -o Dockerfile https://raw.githubusercontent.com/nginxinc/docker-nginx/master/modules/Dockerfile.alpine
-mkdir http-flv-module headers-more-nginx-module
+mkdir http-flv-module && mkdir headers-more-nginx-module
 echo "https://github.com/winshining/nginx-http-flv-module/archive/v1.2.11.tar.gz" > http-flv-module/source
 echo "https://github.com/openresty/headers-more-nginx-module/archive/v0.37.tar.gz" > headers-more-nginx-module/source
